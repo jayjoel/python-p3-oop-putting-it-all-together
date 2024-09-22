26dea8983746f771466965d735d8e8f4063d958c
joel_patrick <jayjoelpn@gmail.com>
2024-09-22 03:06:24 +0300

    initial commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This will use the setter for validation
+        self.condition = "Used"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            raise ValueError("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
